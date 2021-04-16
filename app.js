8bba1c7dae4fff2f51cf288dc306ea5ffa6ef828
Astha Mishra <mishraastha255@gmail.com>
2021-04-16 10:11:50 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+let newItems=["Have Coffee", "Start Working", "Have Breakfast"];
+let workItems=[];
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get("/", function(req,res){
+
+   let today = new Date();
+   let options={
+       weekday:"long",
+       day: "numeric",
+       month: "long",
+   };
+
+   let day=today.toLocaleDateString("en-US",options);
+   
+   res.render("list", {
+      listTitle: day,
+      newListItems:newItems,
+   });
+   
+});
+
+
+app.post("/",function(req,res){
+   let newItem=req.body.newItem;
+
+   if(req.body.list ==="Work List"){
+      workItems.push(newItem);
+      res.redirect("/work");
+   }else{
+      newItems.push(newItem);
+      res.redirect("/");
+   } 
+  
+});
+
+app.get("/work",function(req,res){
+   res.render("list",{
+      listTitle:"Work List",
+      newListItems:workItems,
+   });
+});
+
+
+
+app.listen(3000,function(){
+   console.log("Server started on port 3000");
+});
